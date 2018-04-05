a46cf5ae57b158c6b1d90f945f440d95f421597b
jirokirameki <jirokirameki@yahoo.co.jp>
2018-04-05 01:19:44 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  # resources :users, only: [:show, :new, :create]  
+  resources :users, only: [:show, :create]  
+
 end
